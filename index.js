b089d774ec13ce067cf931d741db00b0719e2d78
Ploo Jompong <ploo.jompong@hotmail.com>
2024-02-09 10:43:12 +0100

    Added placeholder

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
+// API
 const apiKey = "0ba3e5e52a281ae26a09d3e016ae8d1d";
 const method = "flickr.photos.search";
 const baseUrl = "https://api.flickr.com/services/rest";
-const submitBtn = document.querySelector("#input-button");
 
-let textInput = document.querySelector("#search-field");
+// DOM
+const submitBtn = document.querySelector("#input-button");
+const textInput = document.querySelector("#search-field");
+const imageContainer = document.querySelector("#search-results");
+
 let text = "";
 let imgSize = "w"; // 400px
 
+if (text === "") {
+  text = "art";
+  fetchImages();
+}
+
 function fetchImages() {
-  let url = `${baseUrl}?api_key=${apiKey}&method=${method}&text=${text}&per_page=20t f&format=json&nojsoncallback=1`;
+  let url = `${baseUrl}?api_key=${apiKey}&method=${method}&text=${text}&per_page=20&sort=relevance&format=json&nojsoncallback=1`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      // här vill vi hämta vår div med sökresultat
-      const imageContainer = document.querySelector("#search-results");
       // töm containern från tidigare bilder
       imageContainer.innerHTML = "";
+
       data.photos.photo.forEach((img) => {
-        let imgUrl = `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}_${imgSize}.jpg`;
+        const imgUrl = `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}_${imgSize}.jpg`;
+
         // skapa ett nytt img-element för varje bild
-        let imgElement = document.createElement("img");
+        const imgElement = document.createElement("img");
         imgElement.src = imgUrl;
+
         // lägg till nya img-elementet i bildcontainern
         imageContainer.appendChild(imgElement);
-        console.log(imgUrl);
       });
     })
     .catch((error) => {
